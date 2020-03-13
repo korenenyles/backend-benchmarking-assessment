6d7aa9c2e4f9b47b026c028920e46d681c312b49
Koren Nyles <korennyles@pop-os.localdomain>
2020-03-13 12:48:40 -0400

    passed  all tests

--- anagrams.py
+++ anagrams.py
@@ -8,7 +8,7 @@
     for an arbitrary list of strings.
 
 """
-__author__ = "???"
+__author__ = "Koren Nyles, Chris Wilson, Sean Bailey"
 
 import sys
 
@@ -39,20 +39,23 @@ def find_anagrams(words):
         {'dgo': ['dog'], 'act': ['cat', 'act']}
 
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+    anagrams = {}
+    for word in words:
+        if alphabetize(word) in anagrams:
+            anagrams[alphabetize(word)].append(word)
+        # anagrams[alpha_word]=[w for w in words if w == alpha_word]
+        else:
+            anagrams[alphabetize(word)] = [word]
+
     return anagrams
 
 
 if __name__ == "__main__":
     # run find anagrams of first argument
     if len(sys.argv) < 2:
-        print "Please specify a word file!"
+        print("Please specify a word file!")
         sys.exit(1)
     else:
         with open(sys.argv[1], 'r') as handle:
             words = handle.read().split()
-            print find_anagrams(words)
+            print(find_anagrams(words))
